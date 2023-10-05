d1387b77868b901c4ff46e2d376fbfcccf57c583
Grikhanov Maxim <maximw33@yandex.ru>
2023-10-05 23:18:26 +0300

    refactor tests lru

--- pkg/storage/lru/lru_test.go
+++ pkg/storage/lru/lru_test.go
@@ -33,12 +33,7 @@ func TestCreateWithoutTTL(t *testing.T) {
 	lru, err := New(logrus.New(), 1, nil)
 	assert.NoError(t, err, "err create lru assert")
 
-	err = lru.Create("test", "test")
-	assert.NoError(t, err, "err create element assert")
-
-	//because not testing get
-	keys := lru.cache.Keys()
-	assert.EqualValues(t, 1, len(keys), "")
+	createTest(t, lru)
 }
 
 func TestCreateWithTTL(t *testing.T) {
@@ -46,56 +41,68 @@ func TestCreateWithTTL(t *testing.T) {
 	lru, err := New(logrus.New(), 1, &ttl)
 	assert.NoError(t, err)
 
-	err = lru.Create("test", "test")
+	createTest(t, lru)
+}
+
+func createTest(t *testing.T, lru *LRU) {
+	err := lru.Create("test", "test")
 	assert.NoError(t, err)
 
 	//because not testing get
-	keys := lru.cacheWithTTL.Keys()
+	var keys = make([]string, 0)
+	if lru.WithTTL {
+		keys = lru.cacheWithTTL.Keys()
+	} else {
+		keys = lru.cache.Keys()
+	}
+
 	assert.EqualValues(t, 1, len(keys))
 }
 
 func TestGetWithoutTTL(t *testing.T) {
-	var expected = "test"
 	lru, err := New(logrus.New(), 1, nil)
 	assert.NoError(t, err, "err create lru assert")
 
-	err = lru.Create("test", expected)
+	getTest(t, lru)
+}
+
+func TestGetWithTTL(t *testing.T) {
+	ttl := time.Second * 2
+	lru, err := New(logrus.New(), 1, &ttl)
+	assert.NoError(t, err)
+
+	getTest(t, lru)
+}
+
+func getTest(t *testing.T, lru *LRU) {
+	var expected = "test"
+
+	err := lru.Create("test", expected)
 	assert.NoError(t, err, "err create element assert")
 
 	value, err := lru.Get("test")
 	assert.NoError(t, err)
 	assert.EqualValues(t, expected, value)
-
-}
-
-func TestGetWithTTL(t *testing.T) {
-
 }
 
 func TestEvictedWithoutTTL(t *testing.T) {
-	var expected = "evicted"
-
 	lru, err := New(logrus.New(), 1, nil)
 	assert.NoError(t, err, "err create lru assert")
 
-	err = lru.Create("test", "test")
-	assert.NoError(t, err, "err create element assert")
-
-	err = lru.Create("evicted", expected)
-	assert.NoError(t, err, "err create element assert")
-
-	value, err := lru.Get("evicted")
-	assert.NoError(t, err)
-	assert.EqualValues(t, expected, value)
+	evictedTest(t, lru)
 }
 
 func TestEvictedWithTTL(t *testing.T) {
-	var expected = "evicted"
 	ttl := time.Second * 2
 	lru, err := New(logrus.New(), 1, &ttl)
 	assert.NoError(t, err)
 
-	err = lru.Create("test", "test")
+	evictedTest(t, lru)
+}
+
+func evictedTest(t *testing.T, lru *LRU) {
+	var expected = "evicted"
+	err := lru.Create("test", "test")
 	assert.NoError(t, err, "err create element assert")
 
 	err = lru.Create("evicted", expected)
