1012fd0afd4b7f24438068b9f7f70ed0c5c906cf
Grikhanov Maxim <maximw33@yandex.ru>
2023-10-08 03:06:17 +0300

    add locker struct

--- internal/locker/locker.go
+++ internal/locker/locker.go
@@ -1,4 +1,20 @@
 package locker
 
+import (
+	"github.com/MigAru/poseidon/pkg/storage"
+	"github.com/sirupsen/logrus"
+)
+
+//LK - need for lock download image to delete image
 type LK struct {
+	log     logrus.Logger
+	storage storage.ST
 }
+
+type Metadata struct{}
+
+func (lk *LK) Lock(reference string) {}
+
+func (lk *LK) Unlock(reference string) {}
+
+func (lk *LK) Status(reference string) {}
